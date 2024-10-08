42c0a36e41034aef6479d4cc39196df541ed636f
WAHID AHMED <149683090+wahidahmed03@users.noreply.github.com>
2024-10-09 00:44:01 +0600

    Update firebase.js

--- app/firebase.js
+++ app/firebase.js
@@ -1,17 +1,22 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-require('dotenv').config()
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+  apiKey: "AIzaSyATIgU9uZHjEJj0UcASCFuZyiz9J5U7n5s",
+  authDomain: "wahidahmed002-52671.firebaseapp.com",
+  databaseURL: "https://wahidahmed002-52671-default-rtdb.firebaseio.com",
+  projectId: "wahidahmed002-52671",
+  storageBucket: "wahidahmed002-52671.appspot.com",
+  messagingSenderId: "874202066227",
+  appId: "1:874202066227:web:98260b0ce0e1e801cca246",
+  measurementId: "G-5XQHF5ZQJ3"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export default app;
+const analytics = getAnalytics(app);
